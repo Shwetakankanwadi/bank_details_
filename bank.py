01bf2586372c508bb1a49fb0d52fa71203d401b2
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-08 17:13:54 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,15 +1,15 @@
+
 def bank_details(account_number, holder_name, account_type, balance):
     print("\nBank Account Details")
     print("---------------------")
     print("Account Number :", account_number)
     print("Account Holder :", holder_name)
     print("Account Type   :", account_type)
-    print("Balance        : ₹{:.2f}".format(balance))
-
+    print("Balance        :", balance)
 
 acc_no = input("Enter account number: ")
 name = input("Enter account holder name: ")
 acc_type = input("Enter account type: ")
-balance = float(input("Enter account balance: "))
+balance = input("Enter account balance: ")
 
 bank_details(acc_no, name, acc_type, balance)
