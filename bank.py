0f3601a41b0f22508702a42af72674c272f8e263
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-08 17:09:19 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,7 +1,14 @@
-def bank_details(number, name, acc_type, balance):
-    return {
-        "acc_no": number,
-        "name": name,
-        "acc_type": acc_type,
-        "bal": balance
-    }
+def bank_details(account_number, holder_name, account_type, balance):
+    print("\nBank Account Details")
+    print("---------------------")
+    print("Account Number :", account_number)
+    print("Account Holder :", holder_name)
+    print("Account Type   :", account_type)
+    print("Balance        :", balance)
+
+acc_no = input("Enter account number: ")
+name = input("Enter account holder name: ")
+acc_type = input("Enter account type: ")
+balance = input("Enter account balance: ")
+
+bank_details(acc_no, name, acc_type, balance)
