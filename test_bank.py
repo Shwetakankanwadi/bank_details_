2d470ee16737326792d485aef42f9586c2ee5c12
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-08 17:54:42 +0530

    Update test_bank.py

--- test_bank.py
+++ test_bank.py
@@ -1,16 +1,13 @@
-class BankAccount:
-    def __init__(self, name, balance, account_type, account_number):
-        self.name = name
-        self.balance = balance
-        self.account_type = account_type
-        self.account_number = account_number
+from bank import bank_details
 
-    def validate(self):
-        if self.name and self.balance >= 0 and self.account_type in ["Savings", "Current"] and len(self.account_number) == 10:
-            return True
-        else:
-            return False
+def test_bank_details():
+    expected = {
+        "acc_no": 101,
+        "name": "Shweta",
+        "acc_type": "canara",
+        "bal": 800000
+    }
 
+    result = bank_details(101, "Shweta", "canara", 800000)
 
-acc = BankAccount("Shweta", 5000, "Savings", "1234567890")
-print(acc.validate())
+    assert result == expected
