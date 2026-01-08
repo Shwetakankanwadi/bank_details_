9dac0e9735b049b500fcd31f52593441d93ce39b
shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-08 16:01:44 +0530

    Added pytest for bank_details

---
+++ bank.py
@@ -0,0 +1,7 @@
+def bank_details(number, name, acc_type, balance):
+    return {
+        "acc_no": number,
+        "name": name,
+        "acc_type": acc_type,
+        "bal": balance
+    }
---
+++ test_bank.py
@@ -0,0 +1,13 @@
+from bank import bank_details
+
+def test_bank_details():
+    expected = {
+        "acc_no": 10,
+        "name": "Shweta",
+        "acc_type": "canara",
+        "bal": 80000
+    }
+
+    result = bank_details(10, "Shweta", "canara", 80000)
+
+    assert result == expected
