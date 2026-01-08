eb9149afc74986a9e60732a08035447d958d3745
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-08 17:23:25 +0530

    Update test_bank.py

--- test_bank.py
+++ test_bank.py
@@ -2,13 +2,12 @@ from bank import bank_details
 
 def test_bank_details():
     expected = {
-        "acc_no": 10,
-        "acc_no": 10,
+        "acc_no": 101,
         "name": "Shweta",
         "acc_type": "canara",
-        "bal": 80000
+        "bal": 800000
     }
 
-    result = bank_details(10, "Shweta", "canara", 80000)
+    result = bank_details(101, "Shweta", "canara", 80000)
 
     assert result == expected
