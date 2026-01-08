36e3ef2efafb89a0969d15aff2187d2d026f779b
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-08 17:05:02 +0530

    Update test_bank.pys

--- test_bank.py
+++ test_bank.py
@@ -3,6 +3,7 @@ from bank import bank_details
 def test_bank_details():
     expected = {
         "acc_no": 10,
+        "acc_no": 10,
         "name": "Shweta",
         "acc_type": "canara",
         "bal": 80000
