fd9211744948e5eff6dac05ea43ae6d2892c7733
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-08 17:29:36 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,12 +1,16 @@
-acc_no = input("Enter account number: ")
-name = input("Enter name: ")
-acc_type = input("Enter account type: ")
-bal = input("Enter balance: ")
-
 def bank_details(acc_no, name, acc_type, bal):
     return {
-        "acc_no":acc_no,
-        "name":name,
-        "acc_type":acc_type,
-        "bal":bal
+        "acc_no": acc_no,
+        "name": name,
+        "acc_type": acc_type,
+        "bal": bal
     }
+
+if __name__ == "__main__":
+    acc_no = int(input("Enter account number: "))
+    name = input("Enter name: ")
+    acc_type = input("Enter account type: ")
+    bal = int(input("Enter balance: "))
+
+    result = bank_details(acc_no, name, acc_type, bal)
+    print(result)
